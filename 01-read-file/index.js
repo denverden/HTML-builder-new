8f017eba0f139c7e17481831a69eac37c0f9ffd7
DenverDen <denverden@mail.ru>
2024-01-22 09:21:32 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const FS = require('fs');
+const PATH = require('path');
+const FILE_NAME = './text.txt';
+
+FS.createReadStream(PATH.join(__dirname, FILE_NAME), 'utf8').pipe(
+  process.stdout,
+);
