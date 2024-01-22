a0230e066980447fe1a7383e78f28898d93fbd0a
DenverDen <denverden@mail.ru>
2024-01-22 19:05:52 +0300

    fix: fixed 01-read-file file name

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const FS = require('fs');
 const PATH = require('path');
-const FILE_NAME = './text.txt';
+const FILE_NAME = 'text.txt';
 
 FS.createReadStream(PATH.join(__dirname, FILE_NAME), 'utf8').pipe(
   process.stdout,
